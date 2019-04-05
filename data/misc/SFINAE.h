9022be6ec1bc3e4b29f1d2e1012ac3c966bda8ea
Qwe1rty <caleb.rhms@gmail.com>
2019-04-04 23:43:05 -0400

    Some SFINAE stuff

--- data/misc/SFINAE.h
+++ data/misc/SFINAE.h
@@ -5,4 +5,54 @@
 #ifndef CLION_SFINAE_H
 #define CLION_SFINAE_H
 
+#include <type_traits>
+
+/*
+ * Iteration 1
+ */
+//template<typename T>
+//T& print(const T&,
+//         typename std::enable_if<std::is_integral<T>::value, int>::type = nullptr);
+
+/*
+ * Iteration 2
+ * Using std::enable_if_t alias (C++14 feature)
+ */
+//template<typename T>
+//T& print(const T&,
+//         std::enable_if_t<std::is_integral<T>::value, std::nullptr_t> = nullptr);
+
+/*
+ * Iteration 3
+ * Using default template parameters (C++11 feature)
+ */
+//template<typename T, typename std::enable_if_t<std::is_integral<T>::value, std::nullptr_t> = nullptr>
+//T& print(const T&);
+
+/*
+ * Iteration 4
+ * Removed std::nullptr_t template parameter, using void as default enable_if_t parameter (C++11 feature)
+ */
+//template<typename T, typename std::enable_if_t<std::is_integral<T>::value>* = nullptr>
+//T& print(const T&);
+
+/*
+ * Iteration 5
+ * Letting std::enable_if_t void default be the template default (C++11 feature)
+ */
+//template<typename T, typename = std::enable_if_t<std::is_integral<T>::value>>
+//T& print(const T&);
+
+/*
+ * Iteration 6
+ * Separation of conditional
+ */
+template<typename T>
+using enable_if_integral = std::enable_if_t<std::is_integral<T>::value>;
+
+template<typename T, typename = enable_if_integral<T>>
+T& print(const T&);
+
+#include "SFINAE.tpp"
+
 #endif //CLION_SFINAE_H
--- data/misc/SFINAE.tpp
+++ data/misc/SFINAE.tpp
@@ -3,3 +3,11 @@
 //
 
 #include "SFINAE.h"
+
+#include <iostream>
+
+template<typename T>
+T& print(const T& val)
+{
+    std::cout << "is integral type! " << val << std::endl;
+}
