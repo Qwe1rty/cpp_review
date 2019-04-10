1a4b863f4be07bd6edc7aa4d7d5cf33b5c045fc0
Qwe1rty <caleb.rhms@gmail.com>
2019-04-09 21:56:21 -0400

    Unique Paths solution

--- algo/Numbers.cpp
+++ algo/Numbers.cpp
@@ -297,3 +297,30 @@ bool canPartition2(std::vector<int>& nums) {
 
     return dp[sum];
 }
+
+
+/*
+ * Unique Paths
+ * Medium
+ *
+ * speed: 4ms, faster than 100%
+ * memory: 8.7MB, less than 25.41
+ */
+int uniquePaths(int m, int n) {
+
+    auto dp = std::vector<std::vector<int>>(
+        m,
+        std::vector<int>(n, 0)
+    );
+    dp[0][0] = 1;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j != 0) dp[i][j] += dp[i][j - 1];
+            if (i != 0) dp[i][j] += dp[i - 1][j];
+        }
+    }
+
+    return dp[m - 1][n - 1];
+}
+
