f6ac5b81b77ed73890510a4ee886fc69b680ff36
Qwe1rty <caleb.rhms@gmail.com>
2019-04-14 20:44:22 -0400

    Added Longest Increasing Subsequence solution

--- algo/Numbers.cpp
+++ algo/Numbers.cpp
@@ -324,3 +324,28 @@ int uniquePaths(int m, int n) {
     return dp[m - 1][n - 1];
 }
 
+
+/*
+ * Longest Increasing Subsequence
+ * Medium
+ *
+ * Bottom-up O(n^2) DP solution
+ * speed: 40ms, faster than 53.38%
+ * memory: 8.8MB, less than 49.74%
+ */
+int lengthOfLIS(const std::vector<int>& nums) {
+
+    if (nums.size() <= 1) return nums.size();
+
+    int highest = 1;
+    auto dp = std::vector<int>(nums.size(), 1);
+
+    for (int i = 1; i < nums.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+        }
+        if (dp[i] > highest) highest = dp[i];
+    }
+
+    return highest;
+}
