f3e8c32d880c933522894034225cea1ad665d93c
cchoi <caleb.choi@uwaterloo.ca>
2019-05-07 13:37:24 -0400

    Reorganzed solutions and added Cherry Pickup

---
+++ algo/TwoPointers.h
@@ -0,0 +1,29 @@
+//
+// Created by Caleb Choi on 15/04/2019.
+//
+
+#ifndef CLION_TWOPOINTERS_H
+#define CLION_TWOPOINTERS_H
+
+#include <vector>
+
+/*
+ * Medium
+ */
+
+// 11. Container With Most Water
+int maxArea(std::vector<int>& height);
+
+// 18. 4Sum
+std::vector<std::vector<int>> fourSum(std::vector<int>& nums, int target);
+std::vector<std::vector<int>> fourSum2(std::vector<int>& nums, int target);
+
+/*
+ * Hard
+ */
+
+// 42. Trapping Rain Water
+int trap(const std::vector<int>& height);
+
+
+#endif //CLION_TWOPOINTERS_H
